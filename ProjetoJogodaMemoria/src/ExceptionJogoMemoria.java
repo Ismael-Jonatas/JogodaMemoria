f411bacca5c7603cc21cffab17a3d307bd6485d5
ismael jonatas <jhons1808@outlook.com>
2021-09-28 14:34:31 -0300

    UP

---
+++ ProjetoJogodaMemoria/src/ExceptionJogoMemoria.java
@@ -0,0 +1,12 @@
+public class ExceptionJogoMemoria extends RuntimeException{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ExceptionJogoMemoria(String msg) {
+		super(msg);
+	}
+
+}
